437c63e8ede9bf0ff967aed96533944b390cbd69
Khayam Ijaz <ijazkhayam@gmail.com>
2024-02-23 09:51:57 +0500

    creating hooks in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [isNumberAllowed, setIsNumberAllowed] = useState(false);
+  const [isCharacterAllowed, setIsCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState();
+  const passwordGenerator = useCallback(()=>{},[length,isNumberAllowed,isCharacterAllowed,setPassword]);
   return (
     <div className='w-full h-screen bg-zinc-900'>
       
